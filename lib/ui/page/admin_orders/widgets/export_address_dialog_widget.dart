8a4ec2b8a5af11ac09fb5161473f1fc4c141eaba
John Vanderson M Lima <vanderson182@gmail.com>
2021-07-14 10:53:47 -0300

    add captura de endereco

--- lib/ui/page/admin_orders/widgets/export_address_dialog_widget.dart
+++ lib/ui/page/admin_orders/widgets/export_address_dialog_widget.dart
@@ -1,26 +1,41 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/model/address.dart';
+import 'package:screenshot/screenshot.dart';
 
 class ExportAddressDialogWidget extends StatelessWidget {
   final Address address;
+  final ScreenshotController screenshotController = ScreenshotController();
 
-  const ExportAddressDialogWidget({Key? key, required this.address})
+  ExportAddressDialogWidget({Key? key, required this.address})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
       title: const Text('Endereço de Entrega'),
-      content: Text(
-        '${address.street}, ${address.number} ${address.complement}\n'
-        '${address.district}\n'
-        '${address.city}/${address.state}\n'
-        '${address.zipCode}',
+      content: Screenshot(
+        controller: screenshotController,
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          color: Colors.white,
+          child: Text(
+            '${address.street}, ${address.number} ${address.complement}\n'
+            '${address.district}\n'
+            '${address.city}/${address.state}\n'
+            '${address.zipCode}',
+          ),
+        ),
       ),
       contentPadding: const EdgeInsets.fromLTRB(16, 16, 16, 0),
       actions: <Widget>[
         TextButton(
-          onPressed: () {},
+          onPressed: () async {
+            Navigator.of(context).pop();
+            final Uint8List? file = await screenshotController.capture();
+            //await GallerySaver.saveImage(file);
+          },
           child: const Text('Exportar'),
         )
       ],
